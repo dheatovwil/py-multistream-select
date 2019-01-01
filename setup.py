b7a49d1308a40cd30fbc4e21effc1a9b8b55c126
dheatovwil <dheatov.wil@protonmail.com>
2019-01-01 17:20:45 +0800

    Updated packaging metadata and requirements

--- setup.py
+++ setup.py
@@ -19,9 +19,10 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
     description='Multistream select',
     long_description=long_description,
+    long_description_content_type="text/markdown",
 
     # The project's main homepage.
     url='https://github.com/dheatovwil/py-multistream-select',
@@ -50,8 +51,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
